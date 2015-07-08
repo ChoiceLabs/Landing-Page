d875f89fe3c07d2bcbea866e0e4c47ba1b15654b
CallumHoward <callumh@cse.unsw.edu.au>
2015-07-08 15:29:32 -0700

    creating account without firebase auth

--- create_account.js
+++ create_account.js
@@ -1,27 +1,16 @@
 
 var ref = new Firebase("https://choice-dev.firebaseio.com/");
 
-function createAccount(firstName, lastName, phone, city, email, password) {
-  ref.createUser({
-    email    : email,
-    password : password
-  }, function(error, userData) {
-    if (error) {
-      console.log("Error creating user:", error);
-      //TODO properly handle error
-    } else {
-      // save user to firebase workers table //TODO could possibly use transactions
-      var workersRef = ref.child("workers");
-      workersRef.child(userData.uid).set({
-        timeCreated     :   (new Date()).getTime(),
-        signupComplete  :   true,
-        firstName       :   firstName,
-        lastName        :   lastName,
-        email           :   email,
-        phone           :   phone,
-        city            :   city,
-        reputation      :   null,
-      });
-    }
+function createAccount(firstName, lastName, phone, city, email) {
+  var workersRef = ref.child("workers");
+  workersRef.child(userData.uid).set({
+    timeCreated     :   (new Date()).getTime(),
+    signupComplete  :   true,
+    firstName       :   firstName,
+    lastName        :   lastName,
+    phone           :   phone,
+    city            :   city,
+    email           :   email,
+    reputation      :   null,
   });
 }
