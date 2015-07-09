353920621ef51af32498c41fd56f9a52568e400f
CallumHoward <callumh@cse.unsw.edu.au>
2015-07-09 12:08:45 -0700

    createAccount tested and working for checking unique phone and email

--- create_account.js
+++ create_account.js
@@ -3,22 +3,41 @@ var ref = new Firebase("https://choice-dev.firebaseio.com/");
 
 function createAccount(firstName, lastName, phone, city, email) {
   var prospectsRef = ref.child("prospects");
-  var uidRef = prospectsRef.push({
-    timeCreated     :   (new Date()).getTime(),
-    firstName       :   firstName,
-    lastName        :   lastName,
-    phone           :   phone,
-    city            :   city,
-    email           :   email,
-    verified        :   false,
-    reputation      :   null,
+
+  checkUniquePhone(phone, prospectsRef, function (isUnique) {
+    checkUniqueEmail(email, prospectsRef, function (isUnique) {
+      var uidRef = prospectsRef.push({
+        timeCreated     :   (new Date()).getTime(),
+        firstName       :   firstName,
+        lastName        :   lastName,
+        phone           :   phone,
+        city            :   city,
+        email           :   email,
+        verified        :   false,
+        reputation      :   null,
+      });
+      console.log('user added with UID:', uidRef.key());
+    });
   });
-  console.log(uidRef.key());
 }
 
-function testRead() {
-  var prospectsRef = ref.child("prospects");
-  prospectsRef.orderByChild('phone').equalTo("0448378745").once("value", function(snapshot) {
-    console.log(snapshot.exists());
+function checkUniquePhone(phone, ref, callback) {
+  ref.orderByChild("phone").equalTo(phone).once("value", function(snapshot) {
+    if (!snapshot.exists()) {
+      callback();
+    } else {
+      console.log('phone already exists!');
+    }
   });
 }
+
+function checkUniqueEmail(email, ref, callback) {
+  ref.orderByChild("email").equalTo(email).once("value", function(snapshot) {
+    if (!snapshot.exists()) {
+      callback();
+    } else {
+      console.log('email already exists!');
+    }
+  });
+}
+
