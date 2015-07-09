a59e5be4a8c74d6e0b7f05b744aefa0628b23971
CallumHoward <callumh@cse.unsw.edu.au>
2015-07-09 11:13:27 -0700

    true false working

--- create_account.js
+++ create_account.js
@@ -7,7 +7,7 @@ function createAccount(firstName, lastName, phone, city, email) {
     timeCreated     :   (new Date()).getTime(),
     firstName       :   firstName,
     lastName        :   lastName,
-    phone           :   phone+'',
+    phone           :   phone,
     city            :   city,
     email           :   email,
     verified        :   false,
@@ -18,7 +18,7 @@ function createAccount(firstName, lastName, phone, city, email) {
 
 function testRead() {
   var prospectsRef = ref.child("prospects");
-  prospectsRef.orderByChild('phone').equalTo('0443378745').on("value", function(snapshot) {
-    console.log(snapshot.key());
+  prospectsRef.orderByChild('phone').on("child_added", function(snapshot) {
+    console.log(snapshot.child('phone').val() == "0448378745");
   });
 }
