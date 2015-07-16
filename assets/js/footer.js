cd9fe778d6d3906adec842c806fbd10d20fdfa15
CallumHoward <callumh@cse.unsw.edu.au>
2015-07-16 16:41:08 -0700

    deleted redundant footer.js

--- assets/js/footer.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $(window).resize(function() {
-        var footerHeight = $('footer').outerHeight();
-        $('body').css({'marginBottom':'-' + footerHeight + 'px'});
-    });
-    $(window).resize();
-});
