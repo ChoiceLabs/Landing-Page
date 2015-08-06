623b5941e57ec80c21647420fcc24cd2ce395318
Neil Sharma <neilsharma101@gmail.com>
2015-08-06 16:29:35 -0700

    i think i finished validation messages on the lead gen page. Also, added a proper terms of service. Need to generalize checkbox validation for future forms

--- assets/js/form.js
+++ assets/js/form.js
@@ -19,31 +19,89 @@ $(document).ready(function() {
     }
   });
 
+
+
+
+
   /* ----------
-  validation
+  Validation
   ------------ */
 
 
-  // real-time validation styling on visited inputs
+  // real-time validation styling on visited inputs fields
   $("input[type=text], input[type=tel], input[type=email]").focus(function() {
     $(this).addClass('visited');
   });
 
-  // removes error message when valid
-  $('input[required]').keydown(function(event) {
-    var error_message_id = this.name + "-error";
+  // shows error message on input fields
+  // only shows if the following critera are met:
+    // the field has been visited
+    // the field is not focused (error messages are annoying)
+    // the field is invalid (duh)
+
+  $('input[required]').blur(function() {
+
     if (this.checkValidity() == false) {;
       showElementErrorMessage(this);
     }
     else {
       hideElementErrorMessage(this);
     }
+  });
+
+  $('input[required]').keyup(function(event) {
+    if (this.checkValidity() == true) {;
+      hideElementErrorMessage(this);
+    }
+  });
+
+
+
+  function showElementErrorMessage(elem) {
+    var elem_error_message_id = "#" + elem.name + "-error";
+    $(elem_error_message_id).css("display", "block");
+  }
+
+  function hideElementErrorMessage(elem) {
+   var elem_error_message_id = "#" + elem.name + "-error";
+    $(elem_error_message_id).css("display", "none"); 
+  }
+
+  function showValidationErrors() {
+
+    // show validation errors on each field
+    $("input[required").each(function() {
+      if (this.checkValidity() == false) {
+        $(this).addClass("visited");
+        showElementErrorMessage(this);
+      }
+    });
+
+    // show general validation error
+    $(".form-error-message").css('display', 'block');
+  }
+
+  // code specific to terms agreement
+  $("input[name=terms]").click(function() {
+    if ($(this).prop("checked")) {
+      $("#terms-error").css("display", "none");
+      $("#terms-link").css("color", "white");
+    }
+    else {
+      $("#terms-error").css("display", "block")
+       $("#terms-link").css("color", "#333");
+    }
 
   });
 
 
 
-  /* SUBMIT FORM */
+
+
+  /* -----------
+  Submitting Form
+  ----------- */
+
   $("#submit-button").click(function(evt) {
 
     var isValid = true;
@@ -59,6 +117,7 @@ $(document).ready(function() {
     // additional user data grabbed here
     if (isValid) {
 
+
       var data = formatFirebaseData();
 
       // send data to firebase
@@ -68,11 +127,15 @@ $(document).ready(function() {
         // if not successful, display error in thank you modal
         if (error) {
           $(".thank-you-message").html("Whoops, there was an error. Please try again or contact us below");
+          $(".form-error-message").html("Whoa, we had an error. Please contact us");
         }
       });
 
-      // show thank you message
+      // show thank you messages
       $('#thank-you-modal').modal('toggle');
+      $(".form-error-message").html("Thanks! We'll reach out shortly");
+      $(".form-error-message").addClass("form-error-message--success");
+      $("#submit-button").attr("disabled", true);
     }
   
   });
@@ -80,30 +143,10 @@ $(document).ready(function() {
 });
 
 
-function showValidationErrors() {
 
-  $("input[required").each(function() {
-    if (this.checkValidity() == false) {
-      $(this).addClass("visited");
-      showElementErrorMessage(this);
-    }
-  });
-}
-
-function showElementErrorMessage(elem) {
-  var elem_error_message_id = "#" + elem.name + "-error";
-  $(elem_error_message_id).css("display", "block");
-}
-
-function hideElementErrorMessage(elem) {
- var elem_error_message_id = "#" + elem.name + "-error";
-  $(elem_error_message_id).css("display", "none"); 
-}
-
-
-
-
-
+/* -----------
+Firebase
+----------- */
 
 function formatFirebaseData() {
   data = {}
