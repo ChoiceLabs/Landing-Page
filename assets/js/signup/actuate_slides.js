99dca2438e387c426b3028ffe0c4fb0c8f95123a
Neil Sharma <neilsharma101@gmail.com>
2015-08-07 15:03:25 -0700

    deleted unnecessary files

--- assets/js/signup/actuate_slides.js
+++ assets/js/signup/actuate_slides.js
@@ -1,49 +1,20 @@
 $(document).ready(function () {
-  // template context for each slide
-  var slide1_content_context = {
-    'cities': [
-      '',
-      'San Fransisco',
-      'Brooklyn',
-      'Los Angeles',
-      'San Diego',
-      'Seattle',
-      'Washington DC',
-      'Other',
-    ],
-    'checkbox-image': '../images/checkbox.png',
-    'checkbox-text': 'The question goes here',
-  };
 
-  var slide2_content_context = {};
 
-  var slide3_content_context = {
-    'questions': [
-      {
-        'question-text': 'Are you eligible to work in the U.S.?',
-        'toggle-true-text': 'Yes',
-        'toggle-false-text': 'No'
-      },
-      {
-        'question-text': 'Are you willing to undergo a background check?',
-        'toggle-true-text': 'Yes',
-        'toggle-false-text': 'No'
-      },
-      {
-        'question-text': 'Do you have a valid U.S. Driver\'s License?',
-        'toggle-true-text': 'Yes',
-        'toggle-false-text': 'No'
-      }
-    ],
-  };
+  var jobs_hero_context = {
+    "slogan" : "Grab your keys and make more money",
+    "description" : "We offer free consultations to match you with well-paying, flexible driving jobs",
+  }
+  $(".jobs-hero").html(Handlebars.templates.jobs_hero(jobs_hero_context));
 
-  var slide4_content_context = {};
+
+ 
 
   var single_slide_context = {
     "heading" : "Enter your info",
     "caption" : "Just a few questions and you're done!",
     'cities': [
-      'San Fransisco',
+      'SF',
       'Outside SF',
     ],
     "number-of-hours" : [
@@ -60,50 +31,4 @@ $(document).ready(function () {
 
   $(".jobs-form-single-slide").html(Handlebars.templates.single_slide(single_slide_context));
 
-
-
-  var jobs_hero_context = {
-    "slogan" : "Grab your keys and make more money",
-    "description" : "We offer free consultations to match you with well-paying, flexible driving jobs",
-  }
-  $(".jobs-hero").html(Handlebars.templates.jobs_hero(jobs_hero_context));
-
-
-
-
-  // template context for overall slider (renders each slide's content)
-  var slides_context = {
-    'slides': [
-      {
-        'heading': 'Basics',
-        'caption': 'STEP 1 of 4',
-        'content': Handlebars.templates.slide1_content(slide1_content_context),
-      },
-      {
-        'heading': 'Experience',
-        'caption': 'STEP 2 of 4',
-        'content': Handlebars.templates.slide2_content(slide2_content_context),
-      },
-      {
-        'heading': 'Eligibility',
-        'caption': 'STEP 3 of 4',
-        'content': Handlebars.templates.slide3_content(slide3_content_context),
-      },
-      {
-        'heading': 'Availability',
-        'caption': 'STEP 4 of 4',
-        'content': Handlebars.templates.slide4_content(slide4_content_context),
-      }
-    ]
-  };
-
-  var placeholder;
-  for (var i=0; i<slides_context.slides.length; i++) {
-    // header
-    placeholder = '.slide' + (i+1).toString() + '-header-placeholder';
-    $(placeholder).html(Handlebars.templates.slide_header(slides_context.slides[i]));
-    // content
-    placeholder = '.slide' + (i+1).toString() + '-content-placeholder';
-    $(placeholder).html(slides_context.slides[i].content);
-  }
 });
