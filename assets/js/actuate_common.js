63852d31d2662366237bd7e11d310a494ef2cd34
Neil Sharma <neilsharma101@gmail.com>
2015-08-18 16:06:27 -0700

    separated form into its own template, distinct from form section template

--- assets/js/actuate_common.js
+++ assets/js/actuate_common.js
@@ -1,13 +1,18 @@
 $(document).ready(function () {
+
+
   var header_context = {
     'logo_file': 'assets/images/choice_logo2.png'
   };
   $('.header-placeholder').html(Handlebars.templates.header(header_context));
 
+
+
   var footer_context = {
     'copyright':'&copy; Choice 2015 All rights reserved.'
   };
   $('.footer-placeholder').html(Handlebars.templates.footer(footer_context));
+
 
 
   var terms_context = {
@@ -16,13 +21,16 @@ $(document).ready(function () {
   $('.terms-placeholder').html(Handlebars.templates.terms(terms_context));
 
 
+
   var privacy_context = {
   	"privacy" : "this is where the privacy text should go"
   }
   $(".privacy-placeholder").html(Handlebars.templates.privacy(privacy_context));
 
+
+
   var simple_contact_context = {
-  	"email" : "hello@getchoice.io",
+  	"email" : "chris@getchoice.io",
   	"phone" : "123-456-7890"
   }
   $(".simple-contact-placeholder").html(Handlebars.templates.simple_contact(simple_contact_context));
