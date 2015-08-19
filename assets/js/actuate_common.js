d89b1d77f5f667f57c17b2a9f1aaf4c832477796
Neil Sharma <neilsharma101@gmail.com>
2015-08-19 11:39:20 -0700

    fixed sytling on thank you page, fixed styling on hero section of of home page

--- assets/js/actuate_common.js
+++ assets/js/actuate_common.js
@@ -30,6 +30,7 @@ $(document).ready(function () {
 
 
   var simple_contact_context = {
+    "title" : "Contact Us",
   	"email" : "chris@getchoice.io",
   	"phone" : "123-456-7890"
   }
@@ -37,8 +38,8 @@ $(document).ready(function () {
 
 
   var thanks_context = {
-    "title" : "Thanks for signing up!"
-    "message" : "A member of our team will contact you shortly to schedule your free consultation",
+    "title" : "Thanks for signing up!",
+    "message" : "A member of our team will contact you shortly to schedule your free consultation. In the mean time, if you have any questions, please feel free to contact us.",
     "email" : "chris@getchoice.io",
     "phone" : "123-456-7890"
   }
