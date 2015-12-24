df13b4a917e0449b11a2eceb92030c1350ea028c
Neil Sharma <neilsharma101@gmail.com>
2015-12-23 16:08:05 -0800

    added digital marketing signup page. Tested with firebase.

---
+++ assets/js/signup/actuate-digital-marketing-form.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+
+
+  /* ------
+  THE DIGITAL MARKETING SIGNUP FORM PAGE 
+  ------- */
+  var digital_marketing_hero_context = {
+    "slogan" : "Get a $40,000+ Digital Marketing job.",
+    "description" : "The Choice Digital Marketing Training Program helps you land a paid job in just 12 weeks without any prior experience. Our program is centered around an apprenticeship, where you can work on resume-building projects and get mentored by one of our top-notch employers -- many of whom used to work at Google.",
+  }
+  $(".digital-marketing-hero").html(Handlebars.templates.jobs_hero(digital_marketing_hero_context));
+
+
+  // lead gen single-page form
+  var digital_marketing_questions_context =  {
+    "heading" : "Sign up to learn more",
+    "caption" : "Just a few questions and you're done!",
+    "start-dates" : [
+      'Jan 4',
+      'Jan 18',
+    ],
+  }
+  $(".digital-marketing-signup-form").html(Handlebars.templates.digital_marketing_signup(digital_marketing_questions_context));
+
+
+
+
+
+
+
+  /* ------
+  AD-SPECIFIC PAGES
+  ------- */
+
+
+  var munchery_context = {
+    "slogan" : "Make up to $17/hour",
+    "description" : "We match you with flexible driving jobs so you can work when you want and maximize your income. With Munchery, your miles are reimbursed, you get a 50% employee discount on meals, and you have close delivery zones. Approval is fast, easy, and free.",
+  }
+  $(".munchery-hero").html(Handlebars.templates.jobs_hero(munchery_context));
+
+  var order_ahead_context = {
+    "slogan" : "Make up to $25/hour",
+    "description" : "We match you with flexible driving jobs so you can work when you want and maximize your income. With Order Ahead's group orders, you have higher tips and fewer gaps in your shift. Approval is fast, easy, and free.",
+  }
+  $(".order_ahead-hero").html(Handlebars.templates.jobs_hero(order_ahead_context));
+
+  var eat_club_context = {
+    "slogan" : "Make up to $32/hour",
+    "description" : "We match you with flexible driving jobs so you can work when you want and maximize your income. With Eat Club, you enjoy a team atmosphere with paid time off, mileage reimbursement, and complimentary lunch during most shifts. Approval is fast, easy, and free.",
+  }
+  $(".eat_club-hero").html(Handlebars.templates.jobs_hero(eat_club_context));
+
+  var farm_hill_context = {
+    "slogan" : "Make up to $18/hour",
+    "description" : "We match you with flexible driving jobs so you can work when you want and maximize your income. With Farm Hill, miles are covered and you get tips. Plus, free food with every shift! Approval is fast, easy, and free.",
+  }
+  $(".farm_hill-hero").html(Handlebars.templates.jobs_hero(farm_hill_context));
+
+  var sprig_context = {
+    "slogan" : "Make up to $25/hour",
+    "description" : "We match you with flexible driving jobs so you can work when you want and maximize your income. With Sprig, you get free food with every shift and nearby delivery zones to save on gas. Approval is fast, easy, and free."
+  }
+  $(".sprig-hero").html(Handlebars.templates.jobs_hero(sprig_context));
+
+  var chewse_context = {
+    "slogan" : "Make up to $25/hour",
+    "description" : "We match you with flexible driving jobs so you can work when you want and maximize your income. With Chewse, you get free meals when you work. Plus, larger orders means larger tips. Approval is fast, easy, and free.",
+  }
+  $(".chewse-hero").html(Handlebars.templates.jobs_hero(chewse_context));
+
+});
---
+++ assets/js/signup/digital-marketing-application-form.js
@@ -0,0 +1,219 @@
+$(document).ready(function() {
+
+  // save GCLID cookie
+  var gclid = stripTrailingSlash(getURLParameter("gclid"));
+  if (gclid) {
+    document.cookie = "choice_gclid:" + gclid;
+  }
+
+
+
+  /* ---------
+  radio and checkbox button functionality 
+  -----------*/
+  $("input[type=checkbox], input[type=radio]").on("click", function(){
+
+    // if input is a radio item
+    if ( $(this).attr("type") === "radio" ) {
+      $(this).parent().siblings().removeClass("item-selected");
+      $(this).parent().addClass("item-selected");
+    }
+
+    // if input is a checkbox
+    else {
+      $(this).parent().toggleClass("item-selected");
+    }
+  });
+
+
+
+
+  /* ----------
+  Validation
+  ------------ */
+
+
+  // real-time validation styling on visited inputs fields
+  $("input[type=text], input[type=tel], input[type=email]").focus(function() {
+    $(this).addClass('visited');
+  });
+
+  // shows error message on input fields
+  // only shows if the following critera are met:
+    // the field has been visited
+    // the field is not focused (error messages are annoying)
+    // the field is invalid (duh)
+
+  $('input[required]').blur(function() {
+
+    if (this.checkValidity() == false) {;
+      showElementErrorMessage(this);
+    }
+    else {
+      hideElementErrorMessage(this);
+    }
+  });
+
+  $('input[required]').keyup(function(event) {
+    if (this.checkValidity() == true) {;
+      hideElementErrorMessage(this);
+    }
+  });
+
+
+
+  function showElementErrorMessage(elem) {
+    var elem_error_message_id = "#" + elem.name + "-error";
+    $(elem_error_message_id).css("display", "block");
+  }
+
+  function hideElementErrorMessage(elem) {
+   var elem_error_message_id = "#" + elem.name + "-error";
+    $(elem_error_message_id).css("display", "none"); 
+  }
+
+  function showValidationErrors() {
+
+    // show validation errors on each field
+    $("input[required]").each(function() {
+      if (this.checkValidity() == false) {
+        $(this).addClass("visited");
+        showElementErrorMessage(this);
+      }
+    });
+
+    // show general validation error
+    $(".form-error-message").css('display', 'block');
+  }
+
+  // code specific to terms agreement
+  $("input[name=terms]").click(function() {
+    if ($(this).prop("checked")) {
+      $("#terms-error").css("display", "none");
+      $("#terms-link").css("color", "white");
+    }
+    else {
+      $("#terms-error").css("display", "block")
+       $("#terms-link").css("color", "#333");
+    }
+
+  });
+
+
+
+
+
+  /* -----------
+  Submitting Form
+  ----------- */
+
+  $("#submit-button").click(function(evt) {
+
+    var isValid = true;
+    // show errors if form is not valid
+    if ($("#digital-marketing-form")[0].checkValidity() === false) {      
+      showValidationErrors();
+      evt.preventDefault();
+      isValid = false;
+    }
+
+    // if the data is validated,
+    if (isValid) {
+
+      // format data for firebase
+      // additional user data grabbed here
+      var data = formatFirebaseData();
+
+      // send data to firebase
+      //var firebaseRef = new Firebase("https://driverformsignups.firebaseio.com/workerSignups");
+      var firebaseRef = new Firebase("https://choicesignups.firebaseio.com/digital_marketing_student_signups");
+      firebaseRef.push(data, function(error) {
+
+        // if not successful, display error in thank you modal
+        if (error) {
+          $(".thank-you-message").html("Whoops, there was an error. Please try again or contact us below");
+          $(".form-error-message").html("Whoa, we had an error. Please contact us");
+        }
+        else {
+          // show thank you messages
+          //$('#thank-you-modal').modal('toggle');
+          $(".form-error-message").css("display", 'none');
+          $(".form-success-message").toggle();
+
+          // disable submit button
+          $("#submit-button").attr("disabled", true);
+          $("#submit-button").css("background-color", "#ccc");
+          window.location.href = "thanks.html";
+        }
+      });
+      
+      // clear fields
+      /*
+      $("#driver-form")[0].reset();
+      $("input.visited").removeClass("visited");
+      $(".item-selected").removeClass("item-selected");
+      */
+    }
+  
+  });
+
+});
+
+
+
+/* ----------
+QUERY PARAMETERS
+------------*/
+
+function getURLParameter(name) {
+  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
+}
+
+// last parameter sometimes has an appended trailing slash
+function stripTrailingSlash(str) {
+  if (str != null) {
+    if(str.substr(-1) === '/') {
+        return str.substr(0, str.length - 1);
+    }
+    return str;
+  }
+}
+
+
+
+/* -----------
+Firebase
+----------- */
+
+function formatFirebaseData() {
+  data = {}
+
+  data["gclid"] = stripTrailingSlash(getURLParameter("gclid")) || "";
+  data["name"] = $("#name-input").val() || "";
+  data["date"] = Date();
+  data["email"] = $("#email-input").val() ||"";
+  
+  var phone = $("#phone-input").val().replace(/\D/g,'');
+  if (phone.length == 10) {
+    phone = 1 + phone;
+  }
+  data["phone"] = phone || "";
+
+
+  var start_dates =  $("input[name=start-dates]:checked").map(function() {
+    return $(this).val();
+  }).get();
+  if (start_dates.length != 0) {
+    data["start-dates"] = start_dates;
+  }
+
+  var US_eligible = $("input[name=US-eligible]:checked").val();
+  if (US_eligible != null) {
+    data["US_eligible"] = US_eligible;
+  }
+
+  return data;
+
+}
+
+
