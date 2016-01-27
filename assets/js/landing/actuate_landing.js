d74506f8ae71796ad3e03199c12a83495637f19c
Neil Sharma <neilsharma101@gmail.com>
2016-01-26 20:27:46 -0800

    added a lot of copy, created images

--- assets/js/landing/actuate_landing.js
+++ assets/js/landing/actuate_landing.js
@@ -6,25 +6,25 @@ $(document).ready(function () {
   var hero_context = {};
   $('.hero-placeholder').html(Handlebars.templates.hero(hero_context));
 
-  // offerings
+  // why choice
+  var why_choice_context = {};
+  $('.why-choice-placeholder').html(Handlebars.templates.why_choice(why_choice_context));
+
+  // services
   var services_context = {};
   $('.services-placeholder').html(Handlebars.templates.services(services_context));
   
   // our network
   var our_network_context = {}
-  $('.who-we-are-placeholder').html(Handlebars.templates.our_network(our_network_context));
+  $('.our-network-placeholder').html(Handlebars.templates.our_network(our_network_context));
 
   // How Choice Works
   var how_choice_works_context = {};
   $('.how-choice-works-placeholder').html(Handlebars.templates.how_choice_works(how_choice_works_context));
 
+  // 2nd hero
   var getting_started_footer_context = {}
   $('.get-started-placeholder').html(Handlebars.templates.getting_started_footer(getting_started_footer_context));
-
-
-  // footer
-  var navigation_placeholder_context = {}
-  $('.navigation-placeholder').html(Handlebars.templates.footer_navigation(navigation_placeholder_context));
 
 });
 
